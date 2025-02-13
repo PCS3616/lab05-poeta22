779ea2aaf65341fa94d9652a854ca73babf54bdd
poeta22 <pauloficial028@gmail.com>
2025-02-13 02:56:41 -0300

    Add files via upload

--- submission/quadrados-perfeitos.asm
+++ submission/quadrados-perfeitos.asm
@@ -1,37 +1,33 @@
 @ /0000
-START:
-    LD PEND      ; Carrega endereço de escrita
-    AD DOIS      ; Incrementa 2 para começar em 0x102
-    MM PEND      ; Atualiza PEND
-    MM SALVA+1   ; Modifica endereço de `MM 0000`
+START	LD PEND      ; Carrega endereço de escrita    
+	AD DOIS      ; Incrementa 2 para começar em 0x102
+    	MM PEND      ; Atualiza PEND
+    	MM SALVA   ; Modifica endereço de `MM 0000`
 
-    ; Calcula próximo quadrado
-    LD NMENOS    
-    ML DOIS      
-    AD UM        
-    MM NMENOS    
+    	; Calcula próximo quadrado
+    	LD NMENOS    
+    	ML DOIS      
+    	AD UM        
+    	MM NMENOS    
 
-    ; Salva no endereço modificado
-SALVA:
-    MM 0000      
+    	; Salva no endereço modificado
+SALVA	k /0000      
 
-    ; Verifica limite (63²)
-    LD NMENOS    
-    SB LMT       
-    JZ END       
+	; Verifica limite (63²)
+	LD NMENOS    
+	SB LMT       
+	JZ END       
+	JP START     
 
-    JP START     
-
-END:
-    HM /000      
+END	HM /000      
 
 @ /0050
-NMENOS  K = 0000  
-UM      K = 0001  
-DOIS    K = 0002  
-LMT     K = 3969  
-PEND    K = /0100  
+NMENOS	K =0  
+UM      K =1  
+DOIS    K =2  
+LMT     K =3969  
+PEND    K /9100  
 
 @ /0100
-    K = 0000  
+	K =0000  
 
